19b047153f08d16d0bad34c336c7d83d3372a77e
SHAKIL AHMED <ahmedshakil.aiub@gmail.com>
2025-02-06 18:28:36 +0600

    Update index.js

--- src/components/client-view/experience/index.js
+++ src/components/client-view/experience/index.js
@@ -1,5 +1,135 @@
-"use client";
+// "use client";
 
+// import {
+//   Timeline,
+//   TimelineConnector,
+//   TimelineContent,
+//   TimelineDot,
+//   TimelineItem,
+//   TimelineSeparator,
+// } from "@mui/lab";
+// import AnimationWrapper from "../animation-wrapper";
+// import { motion } from "framer-motion";
+
+// export default function ClientExperienceAndEducationView({
+//   educationData,
+//   experienceData,
+// }) {
+//   console.log(educationData, experienceData, "experienceData");
+
+//   return (
+//     <div
+//       className="max-w-screen-xl mt-24 mb-6 sm:mt-14 sm:mb-14 px-6 sm:px-8 lg:px-16 mx-auto"
+//       id="experience"
+//     >
+//       <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-8">
+//         <div className="flex flex-col gap-5">
+//           <AnimationWrapper className={"py-6 sm:py-16"}>
+//             <div className="flex flex-col justify-center items-center row-start-2 sm:row-start-1">
+//               <h1 className="leading-[70px] mb-4 text-3xl lg:text-4xl xl:text-5xl font-medium">
+//                 {"My Experince".split(" ").map((item, index) => (
+//                   <span
+//                     className={`${
+//                       index === 1 ? "text-green-main" : "text-[#000]"
+//                     }`}
+//                   >
+//                     {item}{" "}
+//                   </span>
+//                 ))}
+//               </h1>
+//             </div>
+//           </AnimationWrapper>
+//           <AnimationWrapper>
+//             <div className="flex w-full">
+//               <motion.div className="container">
+//                 <Timeline position="right">
+//                   {experienceData && experienceData.length
+//                     ? experienceData.map((experienceItem) => (
+//                         <TimelineItem>
+//                           <TimelineSeparator>
+//                             <TimelineDot className="bg-green-main" />
+//                             <TimelineConnector className="bg-green-main" />
+//                           </TimelineSeparator>
+//                           <TimelineContent>
+//                             <div className="border-[2px] p-4 rounded-[8px] border-green-main mt-[14px] ml-[16px]">
+//                               <p className="font-bold">
+//                                 {experienceItem.duration}
+//                               </p>
+//                               <h3 className="font-extrabold mt-2">
+//                                 {experienceItem.company},{" "}
+//                                 {experienceItem.location}
+//                               </h3>
+//                               <p className="font-extrabold mt-2">
+//                                 {experienceItem.position}
+//                               </p>
+//                               <p className="font-extralight mt-2">
+//                                 {experienceItem.jobprofile}
+//                               </p>
+//                             </div>
+//                           </TimelineContent>
+//                         </TimelineItem>
+//                       ))
+//                     : null}
+//                 </Timeline>
+//               </motion.div>
+//             </div>
+//           </AnimationWrapper>
+//         </div>
+//         <div className="flex flex-col gap-5">
+//           <AnimationWrapper className={"py-6 sm:py-16"}>
+//             <div className="flex flex-col justify-center items-center row-start-2 sm:row-start-1">
+//               <h1 className="leading-[70px] mb-4 text-3xl lg:text-4xl xl:text-5xl font-medium">
+//                 {"My Education".split(" ").map((item, index) => (
+//                   <span
+//                     className={`${
+//                       index === 1 ? "text-green-main" : "text-[#000]"
+//                     }`}
+//                   >
+//                     {item}{" "}
+//                   </span>
+//                 ))}
+//               </h1>
+//             </div>
+//           </AnimationWrapper>
+//           <AnimationWrapper>
+//             <div className="flex w-full">
+//               <motion.div className="container">
+//                 <Timeline position="right">
+//                   {educationData && educationData.length
+//                     ? educationData.map((educationItem) => (
+//                         <TimelineItem>
+//                           <TimelineSeparator>
+//                             <TimelineDot className="bg-green-main" />
+//                             <TimelineConnector className="bg-green-main" />
+//                           </TimelineSeparator>
+//                           <TimelineContent>
+//                             <div className="border-[2px] p-4 rounded-[8px] border-green-main mt-[14px] ml-[16px]">
+//                               <p className="font-bold">
+//                                 {educationItem.year}
+//                               </p>
+//                               <h3 className="font-extrabold mt-2">
+//                                 {educationItem.college}
+//                               </h3>
+//                               <p className="font-extrabold mt-2">
+//                                 {educationItem.degree}
+//                               </p>
+//                             </div>
+//                           </TimelineContent>
+//                         </TimelineItem>
+//                       ))
+//                     : null}
+//                 </Timeline>
+//               </motion.div>
+//             </div>
+//           </AnimationWrapper>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// 2nd
+"use client";
 import {
   Timeline,
   TimelineConnector,
@@ -15,7 +145,7 @@ export default function ClientExperienceAndEducationView({
   educationData,
   experienceData,
 }) {
-  console.log(educationData, experienceData, "experienceData");
+  // console.log(educationData,experienceData,"educationData");
 
   return (
     <div
@@ -26,19 +156,21 @@ export default function ClientExperienceAndEducationView({
         <div className="flex flex-col gap-5">
           <AnimationWrapper className={"py-6 sm:py-16"}>
             <div className="flex flex-col justify-center items-center row-start-2 sm:row-start-1">
-              <h1 className="leading-[70px] mb-4 text-3xl lg:text-4xl xl:text-5xl font-medium">
+              <h1 className="leading-[70px] mb-4 text-3xl lg:text-4xl xl:text-5xl font-bold">
                 {"My Experince".split(" ").map((item, index) => (
                   <span
                     className={`${
                       index === 1 ? "text-green-main" : "text-[#000]"
                     }`}
                   >
+                    {" "}
                     {item}{" "}
                   </span>
                 ))}
               </h1>
             </div>
           </AnimationWrapper>
+
           <AnimationWrapper>
             <div className="flex w-full">
               <motion.div className="container">
@@ -55,14 +187,14 @@ export default function ClientExperienceAndEducationView({
                               <p className="font-bold">
                                 {experienceItem.duration}
                               </p>
-                              <h3 className="font-extrabold mt-2">
+                              <p className="font-extrabold mt-2">
                                 {experienceItem.company},{" "}
                                 {experienceItem.location}
-                              </h3>
+                              </p>
                               <p className="font-extrabold mt-2">
                                 {experienceItem.position}
                               </p>
-                              <p className="font-extralight mt-2">
+                              <p className="font-bold mt-2">
                                 {experienceItem.jobprofile}
                               </p>
                             </div>
@@ -75,22 +207,27 @@ export default function ClientExperienceAndEducationView({
             </div>
           </AnimationWrapper>
         </div>
+
+        {/* // Education Part Start */}
+
         <div className="flex flex-col gap-5">
           <AnimationWrapper className={"py-6 sm:py-16"}>
             <div className="flex flex-col justify-center items-center row-start-2 sm:row-start-1">
-              <h1 className="leading-[70px] mb-4 text-3xl lg:text-4xl xl:text-5xl font-medium">
+              <h1 className="leading-[70px] mb-4 text-3xl lg:text-4xl xl:text-5xl font-bold">
                 {"My Education".split(" ").map((item, index) => (
                   <span
                     className={`${
                       index === 1 ? "text-green-main" : "text-[#000]"
                     }`}
                   >
+                    {" "}
                     {item}{" "}
                   </span>
                 ))}
               </h1>
             </div>
           </AnimationWrapper>
+
           <AnimationWrapper>
             <div className="flex w-full">
               <motion.div className="container">
@@ -105,13 +242,13 @@ export default function ClientExperienceAndEducationView({
                           <TimelineContent>
                             <div className="border-[2px] p-4 rounded-[8px] border-green-main mt-[14px] ml-[16px]">
                               <p className="font-bold">
-                                {educationItem.year}
+                                Year: {educationItem.year}
                               </p>
-                              <h3 className="font-extrabold mt-2">
-                                {educationItem.college}
-                              </h3>
                               <p className="font-extrabold mt-2">
-                                {educationItem.degree}
+                                College: {educationItem.college}
+                              </p>
+                              <p className="font-bold mt-2">
+                                Degree: {educationItem.degree}
                               </p>
                             </div>
                           </TimelineContent>
